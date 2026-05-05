c76ea7a540da5eb6890a83ab894bada831965ade
BhartiSinghal16 <bhartisinghal763@gmail.com>
2026-05-05 20:51:07 +0530

    fix port for railway

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const journalRoutes = require('./routes/journal');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors({
   origin: [
